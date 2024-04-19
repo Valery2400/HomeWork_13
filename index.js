7c60a45124bf376825440b2fcfb415926608262b
Valery2400 <valeryk@hotmail.com>
2024-04-19 08:34:24 +0200

    first task solved

---
+++ index.js
@@ -0,0 +1,21 @@
+// Задание 
+// Создайте метод getStudent, который возвращает promise - объекта 
+//(студент с именем "Anri" и возрастом 32), c задержкой 4 секунды.
+// Вызовите этот метод - сохраните в переменную.
+// Раскройте promise при помощи then, выведите имя студента.
+
+function getStudent() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const student = { name: 'Anri', age: 32 };
+        resolve(student);
+      }, 4000); 
+    });
+  }
+  
+  const studentPromise = getStudent();
+  
+  studentPromise.then(student => {
+    console.log('Student name:', student.name);  
+});
+  
